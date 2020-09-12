c10da7cd8732f86309cc2b2f01a9aadad77246c0
Christal Vigil <c.vigil27@gmail.com>
2020-09-11 19:47:07 -0600

    updating port

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,6 +14,7 @@ module.exports = function(app) {
         fs.readFile('../Develop/db/db.json', function(error, contents) {
             var dbData = JSON.parse(contents); 
             response.send(dbData); 
+            console.log(dbData); 
         })}); 
 
     //POST REQUEST: Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
@@ -30,3 +31,25 @@ module.exports = function(app) {
         console.log('userid', newUserID); // testing purposes only 
     });  
 }; 
+
+// app.post("/api/notes", (request, response) => {
+//     var userNote = request.body; // create variable to save note to the request body 
+//     var newUserID = db.length; // create variable that is assigned the length of the db.json file 
+//     db.push(userNote);
+//     userNote.newUserID = newUserID + 1; // combine variables above so new id can add to the length of the db.json file 
+    
+//     fs.readFile('../Develop/db/db.json', function (err, data) {
+//         var data = JSON.parse(data);
+//         data.push(userNote); 
+//     fs.writeFile('../Develop/db/db.json', JSON.stringify(data), function (error) {
+//         if (error) throw error; 
+//         console.log("Successfully Written");
+//     })
+//     });
+//     response.json(userNote); // responding (returning) db file to client 
+    
+//     // console.log('dbfile', db); // testing purposes only 
+//     // console.log('note', userNote); // testing purposes only 
+//     // console.log('userid', newUserID); // testing purposes only 
+//     }); 
+// }; 
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express'); // importing express module
 
 var app = express(); //using node module express to create a server 
 
+var PORT = process.env.PORT || 8080;
+
 app.use(express.static('public'));  // server is serving static content & rendering both index and note HTMLs
 
 app.use(express.json()); //middleware to handle data parsing 
@@ -10,5 +12,5 @@ app.use(express.urlencoded({ extended: true}));
 require("./routes/htmlRoutes")(app); // directing the route for the HTML to the server
 require("./routes/apiRoutes")(app);
 
-app.listen(8080, () => console.log('Listening on PORT 8080')); //set up PORT & listener
+app.listen(PORT, () => console.log('Listening on PORT 8080')); //set up PORT & listener
 
